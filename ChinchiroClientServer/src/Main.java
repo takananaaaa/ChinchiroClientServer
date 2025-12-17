1d8abf43c9262a8f0412e6ff2ac061a9084de0b1
BP23090 <bp23090@shibaura-it.ac.jp>
2025-12-17 15:22:09 +0900

    わーるど!

--- ChinchiroClientServer/src/Main.java
+++ ChinchiroClientServer/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
         // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.printf("はろーわーるど!\n");
+        System.out.printf("はろーわーるど!!!\n");
 
         for (int i = 1; i <= 5; i++) {
             //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
